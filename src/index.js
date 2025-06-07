6e768b8ec52b986fa0dcaaaa98360feafba44df7
DavidWells <hello@davidwells.io>
2025-06-06 19:32:57 -0700

    add types

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const chalk = require('chalk')
 const neverNull = require('./never-null')
 
 /**
+ * @typedef {import('chalk').Chalk} Chalk
+ * @typedef {ReturnType<typeof neverNull>} NeverNullProxy
+ */
+
+/**
  * Chalk instance with conditional disable
- * @param  {boolean} noColors - disable chalk colors
- * @return {object} - chalk instance or proxy noOp
+ * @param {boolean} disableColors - disable chalk colors
+ * @returns {Chalk | NeverNullProxy} - chalk instance or proxy noOp
  */
 module.exports = function safeChalk(disableColors) {
   /* if no colors return proxy to chalk API */
--- src/never-null.js
+++ src/never-null.js
@@ -1,33 +1,47 @@
-
 const noOp = () => {}
 
+/**
+ * @param {any} obj - Object to check
+ * @returns {boolean} - Whether the value is a non-null object
+ */
 function isObject(obj) {
   return obj !== null && typeof obj === 'object'
 }
 
-/* Proxy function to ensure chained chalk calls never fail */
+/**
+ * Creates a proxy that ensures chained property access never fails
+ * @template T
+ * @param {T} obj - The object to wrap in a never-null proxy
+ * @returns {((some?: any, none?: () => any) => any) & { [key: string]: any }} - A callable proxy that recursively wraps all accessed properties
+ */
 module.exports = function neverNull(obj) {
   function match(some, none = noOp) {
     if (obj !== null) return some(obj)
     return none()
   }
-  return new Proxy((some, none) => {
+  /** @type {((some?: any, none?: () => any) => any) & { [key: string]: any }} */
+  const proxy = new Proxy((some, none) => {
     if (some) return some // has value return it with no chalk wrapper
     if (!some && !none) return obj
     return match(some, none)
   },
   {
     get: (target, key) => {
-      const obj = target()
+      /** @type {() => any} */
+      const targetFn = target
+      const obj = targetFn()
       if (isObject(obj)) return neverNull(obj[key])
-      return neverNull()
+      return neverNull(null)
     },
     set: (target, key, val) => {
-      const obj = target()
+      /** @type {() => any} */
+      const targetFn = target
+      const obj = targetFn()
       if (isObject(obj)) {
         obj[key] = val
       }
       return true
     },
   })
+  return proxy
 }
